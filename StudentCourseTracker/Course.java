3f860df7d5a1d2d76361a7322ebf3c67e1480a23
Gurkirat Singh <singhgurkirat276@gmail.com>
2025-03-26 14:49:39 -0700

    Update Course.java

--- StudentCourseTracker/Course.java
+++ StudentCourseTracker/Course.java
@@ -1,5 +1,6 @@
 // File: Course.java
 // Just a basic Course class used to represent course data
+// We'll use this to store name, grade, and number of credits for each course
 
 public class Course implements Comparable<Course> {
     String name;
@@ -12,11 +13,13 @@ public class Course implements Comparable<Course> {
         this.credits = credits;
     }
 
+    // used for sorting courses alphabetically
     @Override
     public int compareTo(Course other) {
         return this.name.compareToIgnoreCase(other.name);
     }
 
+    // nice readable format for file saving and printing
     @Override
     public String toString() {
         return name + ", " + grade + ", " + credits;
